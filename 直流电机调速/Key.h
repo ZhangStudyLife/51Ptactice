ed9848076c4aa3ea12c34b66bf7a4f755d52685d
ZhangStudyLife <zyz051013@gmail.com>
2024-09-22 17:04:44 +0800

    2024-09-22-2

--- 直流电机调速/Key.h
+++ 直流电机调速/Key.h
@@ -1,7 +1,11 @@
 #ifndef __KEY_H__
 #define __KEY_H__
 
-unsigned char Key(void);
-void Key_Loop(void);
+// unsigned char Key(void);
+// void Key_Loop(void);
+
+
+unsigned char Key();
+
 
 #endif
--- 直流电机调速/main.c
+++ 直流电机调速/main.c
@@ -4,10 +4,44 @@
 #include "Key.h"
 #include "Nixie.h"
 
+sbit Motor = P1^1;
+unsigned char Conuter,Compare;
+unsigned char KeyNum,Speed;
+
+void Timer0_ISR() interrupt 1
+{   
+    TL0 = 0xA4;				//设置定时初始值
+    TH0 = 0xFF;				//设置定时初始值
+	Conuter++;
+	Conuter%=100;
+	if(Conuter < Compare)
+	{
+		Motor =1;
+	}
+	else
+	{
+		Motor =0;
+	}
+}
+	  
 void main()
 {
+	Timer0_Init();
+	Compare = 0;
+	Nixie(1,0);
 	while(1)
 	{
-		
+		KeyNum=Key();
+		if (KeyNum==1)
+		{
+			Speed++;
+			Speed%=4;
+			Nixie(1,Speed);
+		}
+		if (Speed==0)Compare=0;
+		if (Speed==1)Compare=50;
+		if (Speed==2)Compare=75;
+		if (Speed==3)Compare=100;
+
 	}
 }
